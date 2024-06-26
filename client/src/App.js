39b0874e902a2678dfcc62a2f7fe1c45eca8e91d
Harnoor singh <singhharnoor116@gmail.com>
2024-06-26 08:42:48 +0530

    Basic setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Typography, AppBar } from "@material-ui/core";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h3" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
     </div>
   );
 }
